68e3821827d156734b2e8fe44db5a3988da5757f
SandyBhai03 <kingsamrat94543@gmail.com>
2024-11-21 23:04:52 +0530

    change in some files

---
+++ Practice/Lecture-8/Promise/Questions/app.js
@@ -0,0 +1,20 @@
+// Qns 1. Write a function delay(ms) that returns a Promise which resolves after (ms) milliseconds. Use this function to log "Done!" after 3 seconds.
+
+// function delay(ms){
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// delay(3000).then(res => console.log("Done!"));
+// const p1 = delay(3000)
+// p1.then(res => console.log("Done!"))
+
+// Create a Promise that resolves with the value 10, then chain .then to:
+
+// -----------------------------------
+
+// Qns 2. Create a Promise that resolves with the value 10, then chain .then to: Add 5 to the value.Multiply the result by 2.Log the final result.
+
+
+
+console.log("Radhey Radhey 🙏🙏🙏")
+
